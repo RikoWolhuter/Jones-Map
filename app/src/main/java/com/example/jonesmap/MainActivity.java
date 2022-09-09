baae742e343ae01a82f50ec49694f76e13d48f02
lab_services_student <stormzy56@gmail.com>
2022-09-09 13:16:47 +0200

    Added navigation to the main page, still have to create navigation to the other pages and create the profile page

--- app/src/main/java/com/example/jonesmap/MainActivity.java
+++ app/src/main/java/com/example/jonesmap/MainActivity.java
@@ -1,19 +1,60 @@
 package com.example.jonesmap;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId()){
+            case R.id.nav_map:
+                if(isServicesOK()) {
+                    IntentHelper.openIntent(this, MapActivity.class);
+                }
+                break;
+            case R.id.nav_landmarks:
+                IntentHelper.openIntent(this, LandMarksActivity.class);
+                break;
+            case R.id.nav_settings:
+                IntentHelper.openIntent(this, SettingsActivity.class);
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        //returning true marks the item as selected
+        return true;
+    }
+
+
+    private ImageView img_Sb2;
+    private ImageView img_Sb3;
+
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggleOnOff;
+    private NavigationView navigationView;
+
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
     @Override
@@ -21,19 +62,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        toolbar = findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggleOnOff = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggleOnOff);
+        toggleOnOff.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        img_Sb2 = findViewById(R.id.nav_Landmarks);
+        img_Sb3 = findViewById(R.id.nav_Settings);
+
+
+        img_Sb2.setOnClickListener(this);
+        img_Sb3.setOnClickListener(this);
+
         if(isServicesOK()){
             init();
         }
     }
-    private void init(){
-Button btnMap = (Button) findViewById(R.id.btnMap);
-btnMap.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, MapActivity.class);
-        startActivity(intent);
+
+    public  void onClick(View v)
+    {
+        switch (v.getId()) {
+
+            case R.id.nav_Landmarks:
+                IntentHelper.openIntent(this, LandMarksActivity.class);
+                break;
+            case R.id.nav_Settings:
+                IntentHelper.openIntent(this, SettingsActivity.class);
+                break;
+
+        }
+
     }
-});
+    @Override
+    public void onBackPressed(){
+        //if the drawer is open, close it
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            //otherwise, let the super class handle it
+            super.onBackPressed();
+        }
+    }
+
+    private void init(){
+
+        ImageView img_Sb1 = findViewById(R.id.nav_Map);
+        img_Sb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     public boolean isServicesOK(){
         Log.d(TAG,"isServicesOK: checking google services version");
